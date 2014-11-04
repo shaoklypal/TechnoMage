a02a55d06ecff80717d234884e2c9a026a1d75cd
shaoklypal <shok_kohs@hotmail.com>
2014-11-05 01:07:21 +1300

    added the image for a button in the compedium gui.

--- main/java/shaoklypal/technomage/gui/GuiCompendiumHome.java
+++ main/java/shaoklypal/technomage/gui/GuiCompendiumHome.java
@@ -1,5 +1,7 @@
 package shaoklypal.technomage.gui;
 
+import org.lwjgl.opengl.GL11;
+
 import shaoklypal.technomage.TechnoMage;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
@@ -12,6 +14,14 @@ import net.minecraft.util.ResourceLocation;
 public class GuiCompendiumHome extends GuiContainer {
 	
 	public final ResourceLocation texture1 = new ResourceLocation("techm", "textures/gui/compendiumhome.png");
+	public final ResourceLocation btnHomeUp = new ResourceLocation("techm","textures/gui/btnhomeup.png");
+	public final ResourceLocation btnHomeDown = new ResourceLocation("techm", "textures/gui/btnhomedown.png");
+	public int activeTab = 0;
+	public int y = 0;
+	public int x = 0;
+	public int scale = 10;
+	public float scaleX = (1.0F / (scale));
+	public float scaleY = (1.0F / (scale));
 	
 	public GuiCompendiumHome(InventoryPlayer inventoryPlayer, Item ItemCompendium ) {
 		super(new ContainerCompendium(inventoryPlayer, ItemCompendium ));
@@ -24,9 +34,19 @@ public class GuiCompendiumHome extends GuiContainer {
 	public void drawGuiContainerBackgroundLayer(float var1, int var2, int var3){
 		Minecraft.getMinecraft().getTextureManager().bindTexture(texture1);
 		drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
-		
+		x = guiLeft;
+		y = guiTop;
+		layout();
 	}
 	
-	
+	public void layout(){
+		//x = ((width - xSize) / 2) - 24;
+		switch(activeTab){
+		case 0:
+			GL11.glScalef(scaleX, scaleY, 1F);
+			Minecraft.getMinecraft().getTextureManager().bindTexture(btnHomeDown);
+			drawTexturedModalRect(((x - 25)*scale),(y*scale),0,0,((25*scale)),((25*scale)));
+		}
+	}
 
 }
