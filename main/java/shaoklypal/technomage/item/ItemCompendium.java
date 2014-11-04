1801ca78f7e7973d66f6607e05b2be83941e8aca
shaoklypal <shok_kohs@hotmail.com>
2014-11-04 22:01:04 +1300

    Gui now opening

--- main/java/shaoklypal/technomage/item/ItemCompendium.java
+++ main/java/shaoklypal/technomage/item/ItemCompendium.java
@@ -7,17 +7,26 @@ import shaoklypal.technomage.gui.TechMGuiHandler;
 import shaoklypal.technomage.proxy.ServerProxy;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class ItemCompendium extends Item {
 	
 	public boolean onItemActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ){
-		
+		/*
 		if(!world.isRemote){
 			FMLNetworkHandler.openGui(player, TechnoMage.modInstance, TechnoMage.guiIDCompendium, world, x, y, z);
 			
 		}
+		*/
 		return true;
 	}
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World world, EntityPlayer player){
+		if(!world.isRemote){
+			FMLNetworkHandler.openGui(player, TechnoMage.modInstance, TechnoMage.guiIDCompendium, world,player.chunkCoordX,player.chunkCoordY,player.chunkCoordZ);
+			
+		}
+		return par1ItemStack;
+	  }
 
 }
